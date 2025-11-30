0af6dd2de621022bc6306b9add1b28d7d533b5ec
Poutniiik <fkutka4@gmail.com>
2025-11-30 06:07:13 +0100

    Update web_investice.py

--- web_investice.py
+++ web_investice.py
@@ -10,7 +10,7 @@ import hashlib
 # --- KONFIGURACE ---
 st.set_page_config(page_title="Investiční App", layout="wide", page_icon="📈")
 
-REPO_NAZEV = "Poutniik/Moje-Investice" 
+REPO_NAZEV = "Poutniiik/Moje-Investice" 
 SOUBOR_DATA = "portfolio_data.csv"
 SOUBOR_UZIVATELE = "users_db.csv"
 SOUBOR_HISTORIE = "history_data.csv"
@@ -428,3 +428,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
