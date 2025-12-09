f378640c3d36e40897ce60dd5ce91823a90466d2
Poutniiik <fkutka4@gmail.com>
2025-12-09 16:00:31 +0100

    Update web_investice.py

--- web_investice.py
+++ web_investice.py
@@ -2239,7 +2239,7 @@ def main():
 
     # Čas, kdy se report posílá (600 = 06:00, 1800 = 18:00)
     current_time_int = datetime.now().hour * 100 + datetime.now().minute
-    report_time_int = 1800 # NASTAVENO NA 18:00 PRO TEST
+    report_time_int = 600 # NASTAVENO NA 06:00 PRO TEST
 
     # Pravidlo pro odeslání: 
     # 1. Dnes se ještě neodeslalo 
@@ -3439,5 +3439,6 @@ def render_bank_lab_page():
                 
 if __name__ == "__main__":
     main()
+
 
 
