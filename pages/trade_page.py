cfeee55d375aa28abf4649a9b34d3bc06aaab9ab
Poutniiik <fkutka4@gmail.com>
2025-12-11 10:25:55 +0100

    Update trade_page.py

--- pages/trade_page.py
+++ pages/trade_page.py
@@ -17,8 +17,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # --- KONEC FIXU ---
 
 # Imports z root modulů
-import utils  
-import bank_engine
+from .. import utils 
+from .. import bank_engine
 # ... zbytek souboru
 
 
