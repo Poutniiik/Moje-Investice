1d3cc6e9ccbe39a8bf38c6f4fe8e51886258ad13
Poutniiik <fkutka4@gmail.com>
2025-12-27 02:42:05 +0100

    Update web_investice.py

--- web_investice.py
+++ web_investice.py
@@ -31,7 +31,7 @@ import pytz
 from styles import get_css
 from data_manager import (
     REPO_NAZEV, SOUBOR_DATA, SOUBOR_UZIVATELE, SOUBOR_HISTORIE,
-    SOUBOR_CASH, SOUBOR_VYVOJ, SOUBOR_WATCHLIST, SOUBOR_DIVIDENDY, SOUBOR_STATS, SOUBOR_STRATEGIE, AI_AVAILABLE,
+    SOUBOR_CASH, SOUBOR_VYVOJ, SOUBOR_WATCHLIST, SOUBOR_DIVIDENDY, SOUBOR_STATS, SOUBOR_STRATEGIE,model, AI_AVAILABLE,
     RISK_FREE_RATE,
     get_repo, zasifruj, uloz_csv, uloz_csv_bezpecne, nacti_csv,
     uloz_data_uzivatele, nacti_uzivatele
@@ -3583,6 +3583,7 @@ def render_bank_lab_page():
     
 if __name__ == "__main__":
     main()
+
 
 
 
