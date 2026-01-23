c8d1726316852c20c4977e2af726aec79b30ee88
Poutniiik <fkutka4@gmail.com>
2026-01-23 23:34:05 +0100

    Update ui_pages.py

--- ui_pages.py
+++ ui_pages.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as p
+import pandas as pd
 import numpy as np                 # <--- Nové (pro korelace)
 import yfinance as yf              # <--- Nové (pro stahování dat)
 import plotly.express as px
